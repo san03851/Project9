da70b898d0c5479ac44e5da30a9490086dd9b0cd
san03851 <sangcheul.na@gmail.com>
2026-03-11 13:31:28 +0900

    Function changes
    Added functions instead of hard coding

--- prac0311.cpp
+++ prac0311.cpp
@@ -17,17 +17,39 @@ int length_of_array(char Array[])
 	return length;
 }
 
+void Change_specific_chara_and_Print(char Array[], char SpecificChara, char ChangetoChar, int LengthofArray)
+{
+	int IndexSpec = 0;
+	for (int i = 0; i < LengthofArray; i++)
+	{
+		if (Array[i] == SpecificChara)
+		{
+			Array[i] = ChangetoChar;
+			IndexSpec = i;
+		}
+	}
+	cout << "number of characters in array : ";
+	cout << LengthofArray << '\n';
+	cout << "specific character you are searching for :";
+	cout << Array[IndexSpec] << '\n';
+	cout << "Array of characters : ";
+	for (int i = 0; i < length_of_array(Array); i++)
+	{
+		cout << Array[i] << " ";
+	}
+	cout << "\n";
+	cout << "index number of specific character you are searching for is : ";
+	cout << IndexSpec << '\n';
+}
+
 int main()
 {
 	char Array[6] = { 'a', 'b', 'c', 'd', 'e', '\0'};
-	
-	char* array_pt = Array;
-	int num_char = 0;
-	int index_spec = 0;
-	char User_input;
-	char char_find;
-	//length of array
-	int length = sizeof(Array) / sizeof(Array[0]);
+	char* ArraypPtr = Array;
+	int IndexSpec = 0;
+	char UserInput;
+	char CharFind;
+
 	cout << "Current array is : ";
 	for (int i = 0; i < length_of_array(Array); i++)
 	{
@@ -35,31 +57,30 @@ int main()
 	}
 	cout << "\n";
 	cout << "The character im finding is ";
-	cin >> User_input;
+	cin >> UserInput;
 	cout << "Change specific charater to : ";
-	cin >> char_find;
-	for (int i = 0; i < length; i++)
-	{
-		if (Array[i] == User_input)
-		{
-			*array_pt = Array[i];
-			Array[i] = char_find;
-			index_spec = i;
-		}
-	}
-	//cout << array_pt;
-	cout << "number of characters in array : ";
-	cout << length_of_array(Array) << '\n';
-	cout << "specific character you are searching for :";
-	cout << Array[index_spec] << '\n';
-	cout << "Array of characters : ";
-	for (int i = 0; i < length; i++)
-	{
-		cout << Array[i] << " ";
-	}
-	cout << "\n";
-	cout << "index number of specific character you are searching for is : ";
-	cout << index_spec << '\n';
+	cin >> CharFind;
+	Change_specific_chara_and_Print(Array, UserInput, CharFind, length_of_array(Array));
+	//for (int i = 0; i < length_of_array(Array); i++)
+	//{
+	//	if (Array[i] == UserInput)
+	//	{
+	//		Array[i] = CharFind;
+	//		IndexSpec = i;
+	//	}
+	//}
+	//cout << "number of characters in array : ";
+	//cout << length_of_array(Array) << '\n';
+	//cout << "specific character you are searching for :";
+	//cout << Array[IndexSpec] << '\n';
+	//cout << "Array of characters : ";
+	//for (int i = 0; i < length_of_array(Array); i++)
+	//{
+	//	cout << Array[i] << " ";
+	//}
+	//cout << "\n";
+	//cout << "index number of specific character you are searching for is : ";
+	//cout << IndexSpec << '\n';
 	return 0;
 }
 
