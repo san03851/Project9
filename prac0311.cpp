d5d3a65a4a81222bdb773e8afdaaf68beffa71e6
san03851 <sangcheul.na@gmail.com>
2026-03-11 13:14:58 +0900

    function update
    added length of array func

--- prac0311.cpp
+++ prac0311.cpp
@@ -2,52 +2,48 @@
 
 using namespace std;
 
-class UTexture
+int length_of_array(char Array[])
 {
+	int length = 0;
+	for (int i = 0; i < 1000; i++)
+	{
+		length += 1;
+		if (Array[i] == '\0')
+		{
+			break;
+		}
 
-}; // 
+	}
+	return length;
+}
 
 int main()
 {
-	char Array[5] = { 'a', 'b', 'c', 'd', 'e'};
+	char Array[6] = { 'a', 'b', 'c', 'd', 'e', '\0'};
 	
 	char* array_pt = Array;
 	int num_char = 0;
 	int index_spec = 0;
 	char User_input;
 	char char_find;
-	//for (int i = 0; i < 6; i++)
-	//{
-	//
-	//	if (A[i] == 'c')
-	//	{
-	//		A[i] = '&';
-	//		index_spec = i;
-	//	}
-	//
-	//	num_char += 1;
-	//}
-	//cout << num_char << '\n';
-	//cout << A[index_spec] << '\n';
-	//cout << index_spec << '\n';
-
 	//length of array
 	int length = sizeof(Array) / sizeof(Array[0]);
 	cout << "The character im finding is ";
 	cin >> User_input;
-	cout << "Change specific charater to";
+	cout << "Change specific charater to : ";
 	cin >> char_find;
 	for (int i = 0; i < length; i++)
 	{
 		if (Array[i] == User_input)
 		{
+			*array_pt = Array[i];
 			Array[i] = char_find;
 			index_spec = i;
 		}
-		num_char += 1;
 	}
+	//cout << array_pt;
 	cout << "number of characters in array : ";
-	cout << num_char << '\n';
+	cout << length_of_array(Array) << '\n';
 	cout << "specific character you are searching for :";
 	cout << Array[index_spec] << '\n';
 	cout << "Array of characters : ";
@@ -56,7 +52,7 @@ int main()
 		cout << Array[i];
 	}
 	cout << "\n";
-	cout << "index number of specific character you are searching for";
+	cout << "index number of specific character you are searching for is : ";
 	cout << index_spec << '\n';
 	return 0;
 }
